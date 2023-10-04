4ffcfc800ae4c8ae640ce24a2fba3aadc9872844
Anton <anton.haglund@iths.se>
2023-10-04 12:56:29 +0200

    temporary commit

--- gui/GeoQuizzer/src/App.vue
+++ gui/GeoQuizzer/src/App.vue
@@ -4,13 +4,41 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <div><RouterLink to="/">Home</RouterLink></div>
-  <div><RouterLink to="/register">Register</RouterLink></div>
-  <div><RouterLink to="/map">Map</RouterLink></div>
+<!--  <div><RouterLink to="/">Home</RouterLink></div>-->
+
+<!--  <div><RouterLink to="/map">Map</RouterLink></div>-->
   <!--  WHEN ADDING A NEW LINK ADD HERE AND IN ROUTER -->
+  <div class="headerContent">
+    <div class="profileContent">
+      <img src="" alt="">
+
+    </div>
+    <div class="logo">
+      <h1>Geografi Quiz</h1>
+      <img src="./assets/images/logga.png" alt="">
+    </div>
+    <div class="login">
+    <div class="buttonsTopRight">
+
+      <RouterLink to="/login">Login</RouterLink>
+
+    </div>
+      <div class="buttonsTopRight">
+
+        <RouterLink to="/register">Register</RouterLink>
 
 
+      </div>
+
+    </div>
+
+
+
+
+  </div>
   <div><RouterView/></div>
+
+
 </template>
 
 <style scoped>
--- gui/GeoQuizzer/src/views/HomeView.vue
+++ gui/GeoQuizzer/src/views/HomeView.vue
@@ -3,6 +3,26 @@
 
 <template>
   <main>
+    <div class="mainContent">
+    <div class="quizBox">
+
+      <h2></h2>
+      <img src="" alt="">
+      <h2></h2>
+      <img src="" alt="">
+      <h2></h2>
+      <img src="" alt="">
+
+    </div>
+    </div>
+
     home
   </main>
+  <footer>
+    <div class="footerContent">
+
+
+    </div>
+  </footer>
+
 </template>
