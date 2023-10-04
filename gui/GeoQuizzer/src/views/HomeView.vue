a198e49eedc7f5f76e0732a56bb903142b1e5202
Anton <anton.haglund@iths.se>
2023-10-04 13:31:52 +0200

    Shell version of homepage

--- gui/GeoQuizzer/src/views/HomeView.vue
+++ gui/GeoQuizzer/src/views/HomeView.vue
@@ -5,23 +5,34 @@
   <main>
     <div class="mainContent">
     <div class="quizBox">
-
-      <h2></h2>
-      <img src="" alt="">
-      <h2></h2>
-      <img src="" alt="">
-      <h2></h2>
-      <img src="" alt="">
-
+      <div class="headerBackground">
+      <h2>Place the country</h2>
+      </div>
+      <img src="../assets/images/countries.png" alt="">
     </div>
+
+    <div class="quizBox">
+      <div class="headerBackground"></div>
+      <h2>Learn the flags of the world</h2>
+      <img src="../assets/images/flags.jpg" alt="">
+    </div>
+      <div class="quizBox">
+        <div class="headerBackground">
+      <h2>Learn the capitals of the world</h2>
+      <img src="../assets/images/capitals.png" alt="">
+        </div>
+      </div>
+
+
     </div>
 
     home
   </main>
   <footer>
     <div class="footerContent">
-
-
+    <span>Contact us</span>
+    <span>Patreon</span>
+    <span>Follow us on socials</span>
     </div>
   </footer>
 
