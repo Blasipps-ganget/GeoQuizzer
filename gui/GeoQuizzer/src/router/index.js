c481d405df9d306bc56230cf67b2fd0615261238
AntonLindskog96 <lindskog96@live.se>
2023-10-05 11:49:38 +0200

    added component and view for flags commited

--- gui/GeoQuizzer/src/router/index.js
+++ gui/GeoQuizzer/src/router/index.js
@@ -18,6 +18,11 @@ const router = createRouter({
         path: '/map',
         name: 'map',
         component: () => import('../views/MapView.vue')
+    },
+    {
+      path: '/flag',
+      name: 'flag',
+      component: () => import('../views/FlagView.vue')
     }
   ]
 })
