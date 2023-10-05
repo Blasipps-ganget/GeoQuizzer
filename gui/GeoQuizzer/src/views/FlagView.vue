94043b9083245f251c71429fa278912090d97502
AntonLindskog96 <lindskog96@live.se>
2023-10-05 11:49:38 +0200

    added component and view for flags

---
+++ gui/GeoQuizzer/src/views/FlagView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>From view.vue</h1>
+    <GuessFlagComponent>
+    
+    </GuessFlagComponent>
+  </div>
+</template>
+
+<script>
+import GuessFlagComponent from "../components/GuessFlagComponent.vue"; 
+
+export default {
+  components: {
+  GuessFlagComponent ,
+  },
+};
+</script>
+
+<style scoped>
+
+</style>  
