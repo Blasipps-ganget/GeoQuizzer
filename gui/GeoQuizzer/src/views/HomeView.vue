8695c5a99b9988257a20c7ad5e7c817f1d961296
Anton <anton.haglund@iths.se>
2023-10-06 12:03:51 +0200

    Fix sidebar menu, fix footer, add v-btns.

--- gui/GeoQuizzer/src/views/HomeView.vue
+++ gui/GeoQuizzer/src/views/HomeView.vue
@@ -1,39 +1,132 @@
 <script setup lang="ts">
+import {RouterLink} from "vue-router";
 </script>
 
 <template>
   <main>
+    <div class="centerContent">
     <div class="mainContent">
+
     <div class="quizBox">
       <div class="headerBackground">
-      <h2>Place the country</h2>
+        <RouterLink to="/countries"><h2>Place the country</h2></RouterLink>
       </div>
-      <img src="../assets/images/countries.png" alt="">
+      <RouterLink to="/countries"><img class="selectionImage" src="../assets/images/countries.png" alt=""></RouterLink>
     </div>
 
     <div class="quizBox">
-      <div class="headerBackground"></div>
-      <h2>Learn the flags of the world</h2>
-      <img src="../assets/images/flags.jpg" alt="">
+      <div class="headerBackground">
+      <RouterLink to="/flags"><h2>Learn the flags of the world</h2></RouterLink>
+      </div>
+      <RouterLink to="/flags"><img class="selectionImage" src="../assets/images/flags.jpg" alt=""></RouterLink>
+
     </div>
+      <div class="capitalsdiv">
       <div class="quizBox">
         <div class="headerBackground">
-      <h2>Learn the capitals of the world</h2>
-      <img src="../assets/images/capitals.png" alt="">
+          <RouterLink to="/capitals"><h2>Learn the capitals of the world</h2></RouterLink>
+
+
         </div>
+        <RouterLink to="/capitals">  <img  class="selectionImage"  src="../assets/images/capitals.png" alt=""></RouterLink>
+      </div>
+
       </div>
 
 
     </div>
 
-    home
-  </main>
-  <footer>
-    <div class="footerContent">
-    <span>Contact us</span>
-    <span>Patreon</span>
-    <span>Follow us on socials</span>
     </div>
-  </footer>
+
+
+  </main>
 
 </template>
+<style scoped>
+
+.centerContent {
+
+  margin-top: 40px;
+  justify-content: center;
+  display: flex;
+
+
+}
+
+.mainContent {
+
+  display: grid;
+  gap: 170px;
+  grid-template-columns: 553px 553px;
+  grid-template-rows: 250px 250px ;
+
+
+
+
+  padding-bottom: 180px;
+
+
+
+
+
+
+}
+
+.selectionImage {
+
+  height: 311px;
+  width: 553px;
+  box-shadow: 0px 7px 10px rgba(0,0,0,.4);
+
+
+}
+
+.quizBox {
+
+background: #053B50;
+ box-sizing: border-box;
+
+
+
+
+
+
+
+}
+
+.headerBackground {
+
+
+
+  padding: 20px;
+  text-align: center;
+
+
+
+}
+
+.headerBackground h2 {
+
+  color: #EEEEEE;
+
+}
+.headerBackground a {
+
+  text-decoration: none;
+  color: #EEEEEE;
+}
+
+
+
+
+.capitalsdiv {
+
+  grid-column: span 2;
+  display: flex;
+  justify-content: center;
+
+}
+
+
+
+</style>
