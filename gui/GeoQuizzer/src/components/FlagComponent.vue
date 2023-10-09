99a958b709b3ec2776be057ccaaaa7ca718cb79e
AntonLindskog96 <lindskog96@live.se>
2023-10-09 14:52:00 +0200

    Changed transition on hover on buttons

--- gui/GeoQuizzer/src/components/FlagComponent.vue
+++ gui/GeoQuizzer/src/components/FlagComponent.vue
@@ -45,10 +45,11 @@ onMounted(async () => {
     console.error(error);
   }
 });
-    </script>
-      
-    <style scoped>
+</script>
+
+<style scoped>
     @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap');
+
     * {
       margin: 0;
       padding: 0;
@@ -64,7 +65,7 @@ onMounted(async () => {
     .container {
       width: 50vw;
       max-width: 80%;
-      height: 85vh;
+      height: 70vh;
       box-shadow: 0 0 5px 4px;
       display: flex;
       flex-direction: column;
@@ -80,23 +81,64 @@ onMounted(async () => {
       flex-direction: column;
       padding: 5px;
     }
-   
-    .btn {
 
-      font-size: large;
-      font-weight: bold;
-      box-shadow: 0 6px 12px -2px;
-      font-size: 24px;
-      border-radius: 15px;
-      padding: 10px;
-      margin: 5%;
-      width: 200px;
-      cursor: pointer; 
-    }
-    .btn:hover{
-      color:green;
-      padding: 12px;
-    }
+ 
+.btn {
+  font-size: 20px;
+  font-weight: 700;
+  color: #fff;
+  text-decoration: none;
+  font-family: sans-serif;
+  background-color: yellow;
+  height: 60px;
+  line-height: 60px;
+  text-align: center;
+  width: 200px;
+  padding: 0;
+  position: relative;
+  margin: 5%;
+  z-index: 1;
+  overflow: hidden;
+  border-radius: 10px;
+  margin-top: 20px;
+ 
+}
+      .btn::after, button::before{
+        content: '';
+        background-color: #03d31f;
+        height: 50%;
+        width: 0;
+        position: absolute;
+        transition: 0.3s cubic-bezier(.785, .135, .15, .86);
+        -webkit-transition: 0.3s cubic-bezier(.785, .135, .15, .86);
+        z-index: -1;
+      }
+
+      .btn:hover::before{
+        width: 100%;
+        right: 0;
+        left: auto;
+      }
+
+      .btn::before{
+        top: 0;
+        left: 0;
+        right: auto;
+      }
+
+      .btn:hover::after{
+        width: 100%;
+        left: 0;
+        right: auto;
+      }
+      .btn::after{
+        bottom: 0;
+        right: 0;
+        left: auto;
+      }
+
+
+
     .hide {
       display: none;
     }
@@ -109,3 +151,4 @@ onMounted(async () => {
    
   
     </style>
+      
