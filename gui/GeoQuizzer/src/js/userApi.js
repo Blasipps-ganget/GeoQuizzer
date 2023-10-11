fa391ba8c141711f89bb7d8fc90e270ff717b73c
MikaelEdwartz <Mikael.edwartz@gmail.com>
2023-10-11 17:39:23 +0200

    conflicts

--- gui/GeoQuizzer/src/js/userApi.js
+++ gui/GeoQuizzer/src/js/userApi.js
@@ -1,29 +1,112 @@
+
 const userEndpoint = "http://localhost:8080/user/"
-export const login = (userLog) => {
-    fetch(userEndpoint + 'login', {
+const loginEndpoint = userEndpoint + "login";
+const registerEndpoint = userEndpoint + "signup";
+export const login = async (userLog) => {
+
+    const {accessToken} = getToken();
+    if(accessToken) {
+        const valid = await checkValidity()
+
+        if (!valid) {
+            await refreshToken();
+        }
+    }
+    return fetch(loginEndpoint, {
         method: 'POST',
-        headers: {'Content-Type': 'application/json',},
+        headers: {'Content-Type': 'application/json', 'Authorization': `Bearer ${accessToken}`},
         body: JSON.stringify(userLog)
-    }).then((response) => {
-        console.log(response);
-        return response;
+    }).then(async (response) => {
+        let {token, refreshToken} = await response.json();
+        document.cookie = `accessToken=${token}; path=/;`;
+        document.cookie = `refreshToken=${refreshToken}; path=/;`;
+        return response.status === 200 ? parseJwt(token).name : 'login'
     }).catch((error) => {
-        console.log(error);
+        console.log(error)
+        return false;
     })
 }
 
 export const signUpRest = (userReg) => {
-    fetch(userEndpoint + 'signup', {
+    return fetch(registerEndpoint, {
         method: 'POST',
-        headers: {'Content-Type': 'application/json',},
+        headers: {'Content-Type': 'application/json'},
         body: JSON.stringify(userReg)
-    }).then( (response) => {
-        console.log(response);
+    }).then(async (response) => {
+        if (response.ok) {
+            let {token, refreshToken} = await response.json();
+            document.cookie = `accessToken=${token}; path=/;`;
+            document.cookie = `refreshToken=${refreshToken}; path=/;`;
+            return response.status === 201 ? parseJwt(token).name : 'login'
+
+        }
     }).catch((error) => {
         console.log(error);
+        return false;
     })
 }
 
 
+const getToken = () => {
+    const cookies = document.cookie;
+    const cookieArray = cookies.split('; ');
+
+    let accessToken = null;
+    let refreshToken = null;
+
+    for (const cookie of cookieArray) {
+        const [name, value] = cookie.split('=');
+        if (name === 'accessToken') {
+            accessToken = value;
+        }
+        if (name === 'refreshToken') {
+            refreshToken = value;
+        }
+    }
 
 
+    return {accessToken, refreshToken};
+}
+
+const checkValidity = () => {
+    const {accessToken} = getToken()
+    return fetch(userEndpoint + 'checkValidity', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json', 'Authorization': `Bearer ${accessToken}`},
+    }).then((response) => {
+        return response.status === 200;
+    })
+}
+
+export const refreshToken = () => {
+    const {refreshToken} = getToken()
+    fetch(userEndpoint + 'refresh', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json', 'Authorization': `Bearer ${refreshToken}`},
+    }).then(async (response) => {
+        if (response.ok) {
+            let {accessToken} = await response.json();
+            document.cookie = `accessToken=${accessToken}; path=/;`;
+        }
+    })
+}
+export const isLoggedIn = () => {
+    const {accessToken} = getToken();
+    const payload = parseJwt(accessToken)
+    return payload === null ? 'login' : payload.name
+
+}
+const parseJwt = (token) => {
+    try {
+        return JSON.parse(atob(token.split('.')[1]));
+    } catch (e) {
+        return null;
+    }
+};
+
+/*
+fetch(userEndpoint + 'signup', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json', 'Authorization': `Bearer ${token2}`},
+        body: JSON.stringify(userReg)
+ */
