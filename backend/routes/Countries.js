bbe69caeee58aaad7d9fb9d5582a7c35fff79cde
Christofer Nyström <christofer.nystrom@iths.se>
2023-10-14 13:40:29 +0200

    Separate get quiz and get country in backend

--- backend/routes/Countries.js
+++ backend/routes/Countries.js
@@ -8,6 +8,64 @@ express().use(express.json());
 
 let currentList = [];
 
+router.post("/result", express.json(),  (req, res) => {
+    console.log(req.body.answers)
+    const answers = req.body.answers;
+    compareResults(answers);
+    res.sendStatus(200);
+})
+
+router.get("/:region", (req, res) => {
+const region = req.params.region;
+    if (region === "europe") res.send(getEuropeCountries());
+    else if (region === "asia") res.send(getAsianCountries());
+    else if (region === "oceania") res.send(getOceaniaCountries());
+    else if (region === "africa") res.send(getAfricaCountries());
+    else if (region === "northAmerica") res.send(getNorthAmericaCountries());
+    else if (region === "southAmerica") res.send(getSouthAmericaCountries());
+});
+
+router.get("/quiz/:region", (req, res) => {
+    const region = req.params.region;
+    if (region === "europe") res.send(getEuropeQuiz());
+    else if (region === "asia") res.send(getAsianQuiz());
+    else if (region === "oceania") res.send(getOceaniaQuiz());
+    else if (region === "africa") res.send(getAfricaQuiz());
+    else if (region === "northAmerica") res.send(getNorthAmericaQuiz());
+    else if (region === "southAmerica") res.send(getSouthAmericaQuiz());
+});
+
+
+function getNorthAmericaQuiz() {
+    currentList = shuffle(getNorthAmericaCountries());
+    return currentList;
+}
+
+function getSouthAmericaQuiz() {
+    currentList = shuffle(getSouthAmericaCountries());
+    return currentList;
+}
+
+function getAfricaQuiz() {
+    currentList = shuffle(getAfricaCountries());
+    return currentList;
+}
+
+function getAsianQuiz() {
+    currentList = shuffle(getAsianCountries());
+    return currentList;
+}
+
+function getEuropeQuiz() {
+    currentList = shuffle(getEuropeCountries());
+    return currentList;
+}
+
+function getOceaniaQuiz() {
+    currentList = shuffle(getOceaniaCountries());
+    return currentList;
+}
+
 function shuffle(array) {
     let currentIndex = array.length,  randomIndex;
 
@@ -38,115 +96,62 @@ function compareResults(answers) {
 
 }
 
-router.post("/result", express.json(),  (req, res) => {
-    console.log(req.body.answers)
-    const answers = req.body.answers;
-    compareResults(answers);
-    res.sendStatus(200);
-})
-
-router.get("/:region", (req, res) => {
-const region = req.params.region;
-
-    if (region === "europe") res.send(getEuropeQuiz());
-    else if (region === "asia") res.send(getAsianQuiz());
-    else if (region === "oceania") res.send(getOceaniaQuiz());
-    else if (region === "africa") res.send(getAfricaQuiz());
-    else if (region === "northAmerica") res.send(getNorthAmericaQuiz());
-    else if (region === "southAmerica") res.send(getSouthAmericaQuiz());
-
-});
-
-function getSouthAmericaQuiz() {
-    const southAmericanCountries = [
-        "Argentina", "Bolivia", "Brazil", "Chile", "Colombia",
-        "Ecuador", "Guyana", "Paraguay", "Peru", "Suriname",
-        "Uruguay", "Venezuela", "Falkland Islands" ,"Trinidad and Tobago"
-    ]
-    let shuffledList = shuffle(southAmericanCountries);
-    currentList = shuffledList;
-    return shuffledList;
-}
-
-function getNorthAmericaQuiz() {
-    const northAmericanCountries = [
-        "Canada", "USA", "Mexico", "Greenland",
-        "Belize", "Costa Rica", "El Salvador", "Guatemala", "Honduras",
-        "Nicaragua", "Panama", "The Bahamas",
-        "Cuba",  "Dominican Republic",  "Haiti",
-        "Jamaica", "Puerto Rico",
-
-    ]
-    let shuffledList = shuffle(northAmericanCountries);
-    currentList = shuffledList;
-    return shuffledList;
-}
-
-//  deleted Gambia,
-function getAfricaQuiz() {
-    const africanCountries = [
-        "Algeria", "Angola", "Benin", "Botswana", "Burkina Faso",
-        "Burundi", "Cameroon", "Central African Republic", "Chad",
-        "Democratic Republic of the Congo", "Republic of the Congo", "Djibouti", "Egypt", "Equatorial Guinea",
-        "Eritrea", "Swaziland", "Ethiopia", "Gabon",
-        "Ghana", "Guinea", "Guinea Bissau", "Ivory Coast", "Kenya",
-        "Lesotho", "Liberia", "Libya", "Madagascar", "Malawi",
-        "Mali", "Mauritania", "Morocco", "Mozambique",
-        "Namibia", "Niger", "Nigeria", "Rwanda",
-        "Senegal", "Sierra Leone", "Somalia", "Somaliland", "South Africa",
-        "South Sudan", "Sudan", "United Republic of Tanzania", "Togo", "Tunisia", "Western Sahara",
-        "Uganda", "Zambia", "Zimbabwe"
-        ]
-    let shuffledList = shuffle(africanCountries);
-    currentList = shuffledList;
-    return shuffledList;
-}
-
-// deleted Northern Cyprus
-function getEuropeQuiz() {
-    const europeanCountries = [
-        "Albania", "Austria",
-        "Belarus", "Belgium", "Bosnia and Herzegovina", "Bulgaria",
-        "Croatia", "Cyprus", "Czech Republic", "Denmark", "Estonia", "England",
-        "Finland", "France", "Germany", "Greece",
-        "Hungary", "Iceland", "Ireland", "Italy",
-        "Kosovo", "Latvia", "Lithuania", "Luxembourg",
-        "Moldova", "Montenegro", "Netherlands",
-        "Norway", "Poland", "Portugal", "Romania",
-        "Republic of Serbia", "Slovakia", "Slovenia",
-        "Spain", "Sweden", "Switzerland", "Ukraine" ,"Macedonia"
+function getSouthAmericaCountries() {
+    return [
+        "Argentina", "Bolivia", "Brazil", "Chile", "Colombia", "Ecuador", "Guyana", "Paraguay", "Peru",
+        "Suriname", "Uruguay", "Venezuela", "Falkland Islands" ,"Trinidad and Tobago"
     ];
-    let shuffledList = shuffle(europeanCountries);
-    currentList = shuffledList;
-    return shuffledList;
 }
 
-function getAsianQuiz() {
-    const asianCountries = [
-        "Afghanistan", "Armenia", "Azerbaijan", "Bangladesh",
-        "Bhutan", "Brunei", "Cambodia", "China", "Cyprus",
-        "Georgia", "India", "Indonesia", "Iran", "Iraq",
-        "Israel", "Japan", "Jordan", "Kazakhstan", "Kuwait",
-        "Kyrgyzstan", "Laos", "Lebanon", "Malaysia",
-        "Mongolia", "Myanmar", "Nepal", "North Korea", "Oman",
-        "Pakistan", "Philippines", "Qatar", "Russia",
-        "Saudi Arabia", "South Korea", "Sri Lanka", "Syria",
-        "Taiwan", "Tajikistan", "Thailand", "Turkey", "Turkmenistan",
-        "United Arab Emirates", "Uzbekistan", "Vietnam", "Yemen"
-    ]
-    let shuffledList = shuffle(asianCountries);
-    currentList = shuffledList;
-    return shuffledList;
+function getNorthAmericaCountries() {
+    return [
+        "Canada", "USA", "Mexico", "Greenland", "Belize", "Costa Rica", "El Salvador", "Guatemala", "Honduras",
+        "Nicaragua", "Panama", "The Bahamas", "Cuba",  "Dominican Republic",  "Haiti", "Jamaica", "Puerto Rico",
+    ];
 }
 
-function getOceaniaQuiz() {
-    const oceaniaCountries = [
-        "Australia", "Fiji", "New Zealand", "Papua New Guinea", "Solomon Islands", "Vanuatu"
-    ]
-    let shuffledList = shuffle(oceaniaCountries);
-    currentList = shuffledList;
-    return shuffledList;
+function getAfricaCountries() {
+    //  deleted Gambia,
+    return [
+        "Algeria", "Angola", "Benin", "Botswana", "Burkina Faso", "Burundi", "Cameroon",
+        "Central African Republic", "Chad", "Democratic Republic of the Congo", "Republic of the Congo",
+        "Djibouti", "Egypt", "Equatorial Guinea", "Eritrea", "Swaziland", "Ethiopia", "Gabon", "Ghana",
+        "Guinea", "Guinea Bissau", "Ivory Coast", "Kenya", "Lesotho", "Liberia", "Libya", "Madagascar",
+        "Malawi", "Mali", "Mauritania", "Morocco", "Mozambique", "Namibia", "Niger", "Nigeria", "Rwanda",
+        "Senegal", "Sierra Leone", "Somalia", "Somaliland", "South Africa", "South Sudan", "Sudan",
+        "United Republic of Tanzania", "Togo", "Tunisia", "Western Sahara", "Uganda", "Zambia", "Zimbabwe"
+    ];
 }
+
+function getEuropeCountries() {
+    // deleted Northern Cyprus
+    return [
+        "Albania", "Austria", "Belarus", "Belgium", "Bosnia and Herzegovina", "Bulgaria", "Croatia",
+        "Cyprus", "Czech Republic", "Denmark", "Estonia", "England", "Finland", "France", "Germany",
+        "Greece", "Hungary", "Iceland", "Ireland", "Italy", "Kosovo", "Latvia", "Lithuania",
+        "Luxembourg", "Moldova", "Montenegro", "Netherlands", "Norway", "Poland", "Portugal", "Romania",
+        "Republic of Serbia", "Slovakia", "Slovenia", "Spain", "Sweden", "Switzerland", "Ukraine" ,"Macedonia"
+    ];
+}
+
+function getOceaniaCountries() {
+    return ["Australia", "Fiji", "New Zealand", "Papua New Guinea", "Solomon Islands", "Vanuatu"];
+}
+
+function getAsianCountries() {
+    return [
+        "Afghanistan", "Armenia", "Azerbaijan", "Bangladesh", "Bhutan", "Brunei", "Cambodia", "China", "Cyprus",
+        "Georgia", "India", "Indonesia", "Iran", "Iraq", "Israel", "Japan", "Jordan", "Kazakhstan", "Kuwait",
+        "Kyrgyzstan", "Laos", "Lebanon", "Malaysia", "Mongolia", "Myanmar", "Nepal", "North Korea", "Oman",
+        "Pakistan", "Philippines", "Qatar", "Russia", "Saudi Arabia", "South Korea", "Sri Lanka", "Syria", "Taiwan",
+        "Tajikistan", "Thailand", "Turkey", "Turkmenistan", "United Arab Emirates", "Uzbekistan", "Vietnam", "Yemen"
+    ];
+}
+
+
+
+
+
 
 
 
--- gui/GeoQuizzer/src/views/MapView.vue
+++ gui/GeoQuizzer/src/views/MapView.vue
@@ -10,7 +10,6 @@
     <div class="message" v-if="question">Where is: {{ question }}?</div>
     <div class="message" v-else>Select region</div>
 
-
     <ClickableMap
         :failedGuesses="failedGuesses"
         :succeededGuesses="succeededGuesses"
@@ -72,7 +71,7 @@ async function sleep(ms) {
 async function handleRegionClick(region) {
   if (!region) return;
   selectingRegions.value = false;
-  includedCountries.value = await d3.json(`http://localhost:8080/countries/${region}`);
+  includedCountries.value = await d3.json(`http://localhost:8080/countries/quiz/${region}`);
   question.value = includedCountries.value[0];
 
   // alert(`You clicked ${region}!`);
