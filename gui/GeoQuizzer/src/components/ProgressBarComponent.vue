bd23e29403212b95075079711e73c6ba9e4059eb
Jonathan Råsmark <jonathanrosemark@gmail.com>
2023-10-16 15:14:22 +0200

    Create progress bar

---
+++ gui/GeoQuizzer/src/components/ProgressBarComponent.vue
@@ -0,0 +1,61 @@
+<script setup>
+
+import {defineProps, watch, onMounted} from 'vue'
+const props = defineProps({
+    amountAnswered: {
+      type: Number,
+      required: true
+    },
+    totalQuestions: {
+      type: Number,
+      required: true
+    }
+})
+
+onMounted(() => {
+      const elProgress = document.querySelector("#progressAmount")
+
+      watch(() => props.amountAnswered, (newVal, oldVal) => {
+        if (newVal)
+          elProgress.style.width = ((newVal / props.totalQuestions)*100) + '%';
+
+      })
+
+    }
+)
+</script>
+
+
+<template>
+  <div class="progress">
+    <div id="progressAmount" class="progress__bar"></div>
+  </div>
+</template>
+
+
+<style scoped>
+p { color: red; }
+
+.progress {
+  margin: 50px auto;
+  padding: 2px;
+  width: 100%;
+  max-width: 500px;
+  border: 3px solid #176B87;
+  height: 30px;
+}
+
+.progress .progress__bar {
+  height: 100%;
+  width: 0;
+  background-color: #176B87;
+  /*animation: fill-bar 3s infinite;*/
+}
+
+/*@keyframes fill-bar {
+  from {width: 0%;}
+  to {width: 100%;}
+}*/
+
+
+</style>
