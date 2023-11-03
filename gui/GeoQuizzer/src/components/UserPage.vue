24ac8ddd4a9dc2571b6a8ebe813eaf352deca1c4
Anton <anton.haglund@iths.se>
2023-11-03 10:11:16 +0100

    Fixed conflict

--- gui/GeoQuizzer/src/components/UserPage.vue
+++ gui/GeoQuizzer/src/components/UserPage.vue
@@ -2,24 +2,160 @@
 
 
 
-import CommentList from "@/components/ProfilePage/CommentList.vue";
-import {ref} from "vue";
-import {rules} from "eslint-plugin-vue";
+import CommentList from "@/components/CommentList.vue";
+import {ref, onMounted, computed, watch} from "vue";
+import {useRouter, useRoute} from "vue-router";
+import {useGeneralStore} from "@/stores/general";
+import {handleToken} from "@/js/userApi";
+import {getName, isLoggedIn} from "@/js/userApi";
 
+const route = useRoute();
+const router = useRouter();
+const generalStore = useGeneralStore();
+
+const userName = computed(() => route.params.userId);
 const comments = ref([]);
 const newComment = ref('');
 
-const submitComment = () => {
-
-
-
-    comments.value.push(newComment.value);
-    newComment.value = '';
 
 
 
 
+
+const submitComment = async () => {
+
+    if (!newComment.value) return;
+
+    const commentValue = `${userName.value}: ${newComment.value}`;
+
+
+
+
+
+   // comments.value.push(newComment.value);
+    comments.value.push(commentValue);
+
+
+     await createComment();
+  newComment.value = '';
+
+    await fetchUserData();
+  //  newComment.value = '';
 }
+
+async function getResults(quiztype) {
+  const accessToken = await handleToken();
+  console.log(accessToken)
+  return fetch(`http://localhost:8080/highscores/?name=${userName.value}&quiz=${quiztype}`, {
+    headers: {
+      'Authorization': `Bearer ${accessToken}`
+    },
+    method: 'GET',})
+      .then(response => response.json())
+      //.then(data => data)
+      .catch(error => console.error('There has been a problem with your fetch operation:', error));
+}
+
+let results1 = '';
+await getResults('countries').then((res) => { results1 = res });
+const resultsAsObject1 = JSON.parse(results1);
+const countryResults = resultsAsObject1.highscores;
+
+let results2 = '';
+await getResults('flags').then((res) => { results2 = res });
+const resultsAsObject2 = JSON.parse(results2);
+const flagResults = resultsAsObject2.highscores;
+
+let results3 = '';
+await getResults('capitals').then((res) => { results3 = res });
+const resultsAsObject3 = JSON.parse(results3);
+const capitalResults = resultsAsObject3.highscores;
+
+
+
+
+
+const fetchUserData = async () => {
+
+  try {
+    const s = 'http://localhost:8080/userProfile/getComments/' + route.params.userId
+
+    const response = await fetch(s)
+
+         userName.value = route.params.userId;
+console.log(userName.value);
+    if(response.ok) {
+
+    const data = await response.json();
+
+    console.log("network data", data)
+    comments.value = data;
+
+    } else  {
+
+      throw new Error('Failed to fetch comments')
+    }
+
+  }   catch (error) {
+
+    console.error('Error:', error);
+
+    throw error;
+  }
+
+};
+
+  watch(userName, () => {
+
+    console.log("gg")
+  })
+
+
+
+
+onMounted(() => {
+
+  //userName.value = this.$route.params.userId;
+  fetchUserData();
+
+
+})
+const createComment = async () => {
+  console.log("xcfvgh")
+  try {
+
+    const response = await fetch('http://localhost:8080/userProfile/createComments', {
+        method: 'POST',
+        headers: {
+
+          'Content-Type': 'application/json',
+        },
+      body: JSON.stringify({
+          user_name: userName.value,
+          commented_name: getName(),
+          comment: newComment.value,
+          date: new Date().toISOString(),
+      }),
+
+    });
+
+    if (response.ok) {
+      const data = await response.text();
+      console.log("Response:", data)
+      // comments.value.push(newComment.value)
+      return data;
+
+    } else {
+      throw new Error('Failed to create a comment');
+    }
+  } catch (error) {
+
+    console.error('Error:', error);
+
+    throw error;
+  }
+
+};
 
 </script>
 
@@ -32,21 +168,25 @@ const submitComment = () => {
       <div class="imgContent">
     <img class="userPicture" src="../assets/images/icons/user.png" alt="">
       </div>
-      <h2 class="profileName">Name: Göran5</h2>
+      <h2 class="profileName">Name: {{userName}}</h2>
     </div>
     </div>
     <div class="resultContent">
      <h2 class="statisticName">Statistics:</h2>
-
       <div class="resultItem">
       <img class="imgIcon" src="/src/assets/images/icons/planet-earth.png" alt="">
       <h3 class="quizName">Countries:</h3>
 
-        <span class="continent">Europe: </span>
-        <span class="continent">America: </span>
-        <span class="continent">Asia: </span>
-        <span class="continent">Oceania: </span>
-        <span class="continent">Africa: </span>
+<!--        <span class="continent">Europe: {{regionsData.europe}} % </span>-->
+<!--        <span class="continent">NorthAmerica: {{regionsData.northAmerica}} %  </span>-->
+<!--        <span class="continent">SouthAmerica: {{regionsData.southAmerica}} %  </span>-->
+<!--        <span class="continent">Asia: {{regionsData.asia}} %  </span>-->
+<!--        <span class="continent">Oceania: {{regionsData.oceania}} %  </span>-->
+<!--        <span class="continent">Africa: {{regionsData.africa}} %  </span>-->
+
+        <div class="listRegions" v-for="(item,index) in countryResults" :key="index">
+          {{item.region}} : {{item.percentage}}{{ item.percentage ? '%' : ''}}
+        </div>
       </div>
 
 
@@ -55,11 +195,15 @@ const submitComment = () => {
       <img class="imgIcon" src="/src/assets/images/icons/flag.png" alt="">
       <h3 class="quizName">Flags:</h3>
 
-        <span class="continent">Europe: </span>
-        <span class="continent">America: </span>
-        <span class="continent">Asia: </span>
-        <span class="continent">Oceania: </span>
-        <span class="continent">Africa: </span>
+<!--        <span class="continent">Europe: {{regionsData.europe}} % </span>-->
+<!--        <span class="continent">NorthAmerica: {{regionsData.northAmerica}} %  </span>-->
+<!--        <span class="continent">SouthAmerica: {{regionsData.southAmerica}} %  </span>-->
+<!--        <span class="continent">Asia: {{regionsData.asia}} %  </span>-->
+<!--        <span class="continent">Oceania: {{regionsData.oceania}} %  </span>-->
+<!--        <span class="continent">Africa: {{regionsData.africa}} %  </span>-->
+        <div class="listRegions" v-for="(item,index) in flagResults" :key="index">
+          {{item.region}} : {{item.percentage}}{{ item.percentage ? '%' : ''}}
+        </div>
       </div>
 
 
@@ -69,11 +213,15 @@ const submitComment = () => {
       <img src="" alt="">
       <h3 class="quizName">Capitals:</h3>
 
-        <span class="continent">Europe: </span>
-        <span class="continent">America: </span>
-        <span class="continent">Asia: </span>
-        <span class="continent">Oceania: </span>
-        <span class="continent">Africa: </span>
+<!--        <span class="continent">Europe: {{regionsData.europe}} % </span>-->
+<!--        <span class="continent">NorthAmerica: {{regionsData.northAmerica}} %  </span>-->
+<!--        <span class="continent">SouthAmerica: {{regionsData.southAmerica}} %  </span>-->
+<!--        <span class="continent">Asia: {{regionsData.asia}} %  </span>-->
+<!--        <span class="continent">Oceania: {{regionsData.oceania}} %  </span>-->
+<!--        <span class="continent">Africa: {{regionsData.africa}} %  </span>-->
+        <div class="listRegions" v-for="(item,index) in capitalResults" :key="index">
+          {{item.region}} : {{item.percentage}}{{ item.percentage ? '%' : ''}}
+        </div>
       </div>
     </div>
 <!--    <div class="comment-section">-->
@@ -105,9 +253,10 @@ const submitComment = () => {
           <v-text-field
               v-model="newComment"
               label="Comment"
-              :rules="[rules.required]"
           ></v-text-field>
-          <v-btn type="submit" block class="mt-2">Submit</v-btn>
+          <div class="btnContent">
+          <v-btn :disabled="!newComment || generalStore.isloggedin" class="submitBtn" type="submit" >Submit</v-btn>
+          </div>
         </v-form>
       </v-sheet>
 
@@ -128,8 +277,72 @@ const submitComment = () => {
 
 <style scoped>
 
+@media screen and (max-width: 800px) {
+
+  .userContent {
+
+    display: flex;
+
+
+  width: 400px;
+
+
+
+  }
+
+  .userContainer {
+
+    height: fit-content;
+    width: fit-content;
+  }
+
+  .resultContent {
+
+
+    flex-direction: row-reverse;
+    margin-right: auto;
+
+  }
+
+  .resultItem {
+
+
+  }
+
+
+}
+
+
+
+.listRegions {
+
+  margin-right: 20px;
+  font-size: large;
+  margin-bottom: 1px;
+  //border: solid 2px white;
+
+
+
+
+}
+
+.btnContent {
+
+
+  display: flex;
+  flex-direction: column;
+}
+
+.submitBtn {
+
+  background-color: #053B50;
+  color: #EEEEEE;
+
+
+}
 .commentsAmount {
 
+  color: #053B50;
 }
 
 
@@ -139,6 +352,7 @@ margin-top: 20px;
   display: flex;
   margin-left: 20px;
   flex-direction: column;
+
 
 }
 
@@ -150,7 +364,9 @@ margin-top: 20px;
 
 .resultItem {
 
-  margin-top: 20px;
+  margin-top: 50px;
+  //display: flex;
+
 }
 
 .imgIcon {
@@ -170,6 +386,9 @@ margin-top: 20px;
   padding: 20px;
   border-radius: 10px;
   background-color: #176B87;
+  font-weight: bold;
+  display: flex;
+
 
 }
 
@@ -187,8 +406,10 @@ margin-top: 20px;
   padding: 40px;
   margin-top: 20px;
   border-radius: 10px;
-  background-color: #EEEEEE;
+  background-color: #053B50;
   color: #EEEEEE;
+  height: 600px;
+
 
 
 
@@ -201,18 +422,21 @@ margin-top: 20px;
 }
 .statisticName {
 
-  margin-bottom: 0px;
+  margin-left: auto;
 }
 
 .quizName {
 
 
-
+margin-bottom: 10px;
 }
 
 .commentName {
 
   margin-top: 10px;
+  color: #053B50;
+  font-weight: bolder;
+
 }
 
 .formBox {
@@ -237,7 +461,7 @@ margin-top: 20px;
   margin: -40px;
   border-radius: 10px;
 
-  background-color: #053B50;
+  background-color: #176B87;
 
 }
 .userPicture {
